a8de0b00cde30ef22618d11eb1cf8159a3d851dd
Jimmy Huang <jun@huang.sh>
2018-06-01 15:35:37 +0800

    Update ci.sh
    revert ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
-rm dist/*.js
-yarn build
 
-echo "Please edit index.html and index_m.html to include updated app app_m manifest and vendor files."
+yarn build && yarn predeploy &&  electron-packager . CPSWallet --asar=true  --platform=win32,darwin,linux --arch=x64  --overwrite --out=./outapp/ --no-prune  --icon CPSWALLETLOGO
+
+cd outapp/
+rm *.zip
+zip -r  CPSWallet-linux-x64.zip CPSWallet-linux-x64
+zip -r  CPSWallet-darwin-x64.zip CPSWallet-darwin-x64
+zip -r  CPSWallet-win32-x64.zip CPSWallet-win32-x64
+cd ..
+
+npm run setup
+
+python upload.py outapp/CPSWallet-win32-x64.zip CPSWallet-win32-x64.zip
+python upload.py outapp/installers/CPSWallet-0.0.1-setup.exe  CPSWallet-0.0.1-setup.exe
+python upload.py outapp/CPSWallet-linux-x64.zip  CPSWallet-linux-x64.zip
+python upload.py outapp/CPSWallet-darwin-x64.zip  CPSWallet-darwin-x64.zip
